65e4ada4a0262e99e438e043a7eb982a85c47e98
jls <jls.smt@gmail.com>
2021-08-11 00:55:29 +0200

    Revert "test CI"
    This reverts commit b16f47250c9c918b3a5f5abe916990d24bce523e.

--- admin/src/components/settings.tsx
+++ admin/src/components/settings.tsx
@@ -131,7 +131,6 @@ class Settings extends React.Component<SettingsProps, SettingsState> {
             <form className={this.props.classes.tab}>
                 {this.renderCheckbox('option1', 'option1')}<br />
                 {this.renderInput('option2', 'option2', 'text')}
-
             </form>
         );
     }
